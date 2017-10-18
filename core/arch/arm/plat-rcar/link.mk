a3dde94419dce526318c90c49866197e817ae792
Volodymyr Babchuk <vlad.babchuk@gmail.com>
2017-10-18 14:28:56 +0300

    LOCAL: Create tee-raw.bin
    This binary is needed to flash tee using Lauterbach.

--- core/arch/arm/plat-rcar/link.mk
+++ core/arch/arm/plat-rcar/link.mk
@@ -7,3 +7,10 @@ cleanfiles += $(link-out-dir)/tee.srec
 $(link-out-dir)/tee.srec: $(link-out-dir)/tee-whole_v2.bin
 	@$(cmd-echo-silent) '  GEN     $@'
 	$(q)$(OBJCOPYcore) -I binary -O srec $< $@
+
+all: $(link-out-dir)/tee_raw.bin
+cleanfiles += $(link-out-dir)/tee_raw.bin
+$(link-out-dir)/tee_raw.bin: $(link-out-dir)/tee.elf
+	@$(cmd-echo-silent) '  GEN     $@'
+	$(q)$(OBJCOPYcore) -O binary $< $@
+
