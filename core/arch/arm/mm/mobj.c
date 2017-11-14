6a2f3a48c818261b10f17da4bb764c5496a6f56e
Volodymyr Babchuk <vlad.babchuk@gmail.com>
2017-11-14 18:16:51 +0200

    mobj: use correct type to hold exceptions state
    cpu_spin_lock_xsave() returns exceptions state in uin32_t, not
    in unsigned int.x
    
    Signed-off-by: Volodymyr Babchuk <vlad.babchuk@gmail.com>

--- core/arch/arm/mm/mobj.c
+++ core/arch/arm/mm/mobj.c
@@ -484,7 +484,7 @@ err:
 struct mobj *mobj_reg_shm_find_by_cookie(uint64_t cookie)
 {
 	struct mobj_reg_shm *mobj_reg_shm;
-	unsigned int exceptions;
+	uint32_t exceptions;
 
 	exceptions = cpu_spin_lock_xsave(&reg_shm_slist_lock);
 	SLIST_FOREACH(mobj_reg_shm, &reg_shm_list, next) {
